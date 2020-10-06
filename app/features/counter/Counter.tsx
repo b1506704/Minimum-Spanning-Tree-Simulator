ac18b023deec315dc04b84a381bf14f4a1864c5b
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2020-10-07 03:07:46 +0700

    Added new and load page

--- app/features/counter/Counter.tsx
+++ app/features/counter/Counter.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from './Counter.css';
 import routes from '../../constants/routes.json';
-import {
-  increment,
-  decrement,
-  incrementIfOdd,
-  incrementAsync,
-  selectCount,
-} from './counterSlice';
+// import {
+//   increment,
+//   decrement,
+//   incrementIfOdd,
+//   incrementAsync,
+//   selectCount,
+// } from './counterSlice';
 
 export default function Counter() {
-  const dispatch = useDispatch();
-  const value = useSelector(selectCount);
+  // const dispatch = useDispatch();
+  // const value = useSelector(selectCount);
   return (
     <div className={styles.background}>
       <div className={styles.backButton} data-tid="backButton">
@@ -21,7 +21,40 @@ export default function Counter() {
           <i className="fa fa-arrow-left fa-3x" />
         </Link>
       </div>
-      <div className={`counter ${styles.counter}`} data-tid="counter">
+      <div className={styles.box}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+          <line className={styles.top} x1="0" y1="0" x2="900" y2="0" />
+          <line className={styles.left} x1="0" y1="460" x2="0" y2="-920" />
+          <line
+            className={styles.bottom}
+            x1="300"
+            y1="460"
+            x2="-600"
+            y2="460"
+          />
+          <line className={styles.right} x1="300" y1="0" x2="300" y2="1380" />
+        </svg>
+        <h3>New Graph</h3>
+        <span> __________ </span>
+        <span> Start a new route </span>
+      </div>
+      <div className={styles.box}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+          <line className={styles.top} x1="0" y1="0" x2="900" y2="0" />
+          <line className={styles.left} x1="0" y1="460" x2="0" y2="-920" />
+          <line
+            className={styles.bottom}
+            x1="300"
+            y1="460"
+            x2="-600"
+            y2="460"
+          />
+          <line className={styles.right} x1="300" y1="0" x2="300" y2="1380" />
+        </svg>
+        <h3>Load Graph</h3>
+        <span>Continue a route</span>
+      </div>
+      {/* <div className={`counter ${styles.counter}`} data-tid="counter">
         {value}
       </div>
       <div className={styles.btnGroup}>
@@ -65,7 +98,7 @@ export default function Counter() {
         >
           async
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
