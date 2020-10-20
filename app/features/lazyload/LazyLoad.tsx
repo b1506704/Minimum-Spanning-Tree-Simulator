3d34e65a726284ce742e6467433bf704caf5afc7
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2020-10-21 02:25:03 +0700

    add lazypage animation

--- app/features/lazyload/LazyLoad.tsx
+++ app/features/lazyload/LazyLoad.tsx
@@ -2,5 +2,18 @@ import React from 'react';
 import styles from './LazyLoad.css';
 
 export default function LazyLoad(): JSX.Element {
-  return <div className={styles.container}>aa</div>;
+  function transition() {
+    // const topOverlay = document.getElementById('top_overlay');
+    // const bottomOverlay = document.getElementById('bottom_overlay');
+    // access deep nest value
+    // if (bottomOverlay) bottomOverlay.style.zIndex = '999';
+    // if (topOverlay) topOverlay.style.zIndex = '999';
+  }
+  return (
+    <div className={styles.container} id="container" onMouseEnter={transition}>
+      <div className={styles.top_overlay} id="top_overlay" />
+      <div className={styles.loading_icon}> </div>
+      <div className={styles.bottom_overlay} id="bottom_overlay" />
+    </div>
+  );
 }
